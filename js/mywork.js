d33ee940f1f6f53191368794b27c51e479f919e0
Gautham V <gauthamv6002@gmail.com>
2021-12-04 08:46:02 -0500

    Only one img shown

--- js/mywork.js
+++ js/mywork.js
@@ -1,14 +1,29 @@
-const tipi = $(".tipi")
-const astrodome = $(".astrodome")
-const aliasThis = $(".this")
+const tipi = $(".tipi"); // 0
+const astrodome = $(".astrodome"); // 1
+const aliasThis = $(".this"); // 2
 
+const clickers = [tipi, astrodome, aliasThis];
 
+const setImg = (clicker, shouldFade) => {
+    if(shouldFade){
+        clickers.map(c => {
+            if(c !== clicker){
+                c.fadeOut();
+            }
+        });
+    } else {
+        clickers.map(c => {
+            if(c !== clicker){
+                c.hide();
+            }
+        });
+    }
+    
+}
 
 $(document).ready(() => {
 
-    whereAmI.click(() => {
-
-    })
+    setImg(tipi, false);
 
 
 
